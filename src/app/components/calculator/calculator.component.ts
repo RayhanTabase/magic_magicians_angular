f0833f63ede91f6783fb55fdcba2e5bc10e840ed
Rayhan <nofussdufus@gmail.com>
2022-01-07 14:58:32 +0000

    Add math functionality to calculator

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { calculate } from './calculate';
+
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-  
+  total:string = '';
+  next:string = '';
+  operation:string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleClick(event: MouseEvent) {
+    let data: {[key: string]: string} = {
+      total: this.total,
+      next: this.next,
+      operation:this.operation,
+    }
+    const element = event.currentTarget as HTMLInputElement;
+    const name = element.name;
+    console.log(name);
+    const result = calculate(data,name);
+    this.total = result.total;
+    this.next = result.next;
+    this.operation = result.operation;
+    console.log(result);
+  }
 }
