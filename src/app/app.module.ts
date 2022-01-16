a518d0408890ede8e3b1ced756d9798b0fc55ca8
Rayhan <nofussdufus@gmail.com>
2022-01-16 10:59:25 +0000

    Add home,quote ,and navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router'
+
 import { CalculatorComponent } from './components/calculator/calculator.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { QuoteComponent } from './components/quote/quote.component';
+
+// const appRoutes: Routes = [
+//   {path: '', component:}
+// ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    HomeComponent,
+    NavbarComponent,
+    QuoteComponent,
   ],
   imports: [
     BrowserModule,
